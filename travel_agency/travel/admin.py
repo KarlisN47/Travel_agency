24bbd3e9d6578a9213bd555ff30a9ff67f6fbabc
Karlisn47 <Karlisn47@gmail.com>
2020-08-15 10:06:55 +0300

    Git repo update.

--- travel_agency/travel/admin.py
+++ travel_agency/travel/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from travel.models import Continent, Country, City, Hotel, Airport, Trip, Global, SocialMedia
 
-# Register your models here.
+admin.site.register(Continent)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Hotel)
+admin.site.register(Airport)
+admin.site.register(Trip)
+admin.site.register(Global)
+admin.site.register(SocialMedia)
--- travel_agency/travel/urls.py
+++ travel_agency/travel/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from travel.views import BaseView
+from travel.views import BaseView, BaseView2, DetailTripView, TripListView, EuropeTripView, TrippListView, signup_view
 from . import views
 urlpatterns = [
-    path('', BaseView.as_view(), name='home')
+    path('', BaseView.as_view(), name='home'),
+    path('index/', BaseView2.as_view(), name='index'),
+    path('detail/<int:pk>', DetailTripView.as_view(), name='detail'),
+    path('global/', TripListView.as_view(), name='global'),
+    path('europe/', EuropeTripView.as_view(), name='europe'),
+    path('list/', TrippListView.as_view(), name='list'),
+    path('signup/', signup_view, name="signup"),
 ]
